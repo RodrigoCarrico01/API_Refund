aac75a8bba985e1c963203e75d3c287e03d643f0
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-06-02 17:01:34 +0100

    feat: authentication working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,18 @@ import { Router } from "express"
 
 import { usersRoutes } from "./users-routes"
 import { sessionsRoutes } from "./sessions-routes"
+import { refundsRoutes } from "./refunds-routes"
+
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 
 const routes = Router()
 
+//PUBLIC ROUTES
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
+
+//PRIVATE ROUTES
+routes.use(ensureAuthenticated)
+routes.use("/refunds", refundsRoutes)
 
 export {routes}
