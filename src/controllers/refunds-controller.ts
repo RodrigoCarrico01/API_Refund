d4212d6528fc97c3ca7accad97301268814ddc5d
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-06-03 10:08:12 +0100

    feat: listing refunds and pagination added

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express"
+import { Request, Response} from "express"
 import {z} from "zod"
 import {prisma} from "@/database/prisma"
 import { AppError } from "@/utils/AppError"
@@ -30,6 +30,57 @@ class RefundsController {
     })
 
     response.status(201).json(refund)
+  }
+  async index(request: Request, response: Response){
+    const querySchema = z.object({
+      name: z.string().optional().default(""),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
+
+    })
+
+    const {name, page, perPage} = querySchema.parse(request.query)
+
+    const skip = (page - 1) * perPage 
+
+    const refunds = await prisma.refunds.findMany({
+      skip,
+      take: perPage,
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          }
+        }
+      },
+      orderBy: {
+        createdAt: "desc"
+      },
+      include: {
+        user: true
+      }
+    })
+    const totalRecords = await prisma.refunds.count({
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          }
+        }
+      },
+    })
+
+    const totalPages = Math.ceil(totalRecords / perPage)
+
+    response.json({
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1,
+      }
+    })
   }
 }
 
--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -7,7 +7,8 @@ import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization
 const refundsRoutes = Router()
 const refundsController = new RefundsController()
 
-
 refundsRoutes.post("/", verifyUserAuthorization(["employee"]), refundsController.create)
+refundsRoutes.get("/", verifyUserAuthorization(["manager"]), refundsController.index)
+
 
 export {refundsRoutes}
