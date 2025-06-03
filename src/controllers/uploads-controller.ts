fe3364d3093bbac93e7fafa51b4839800e5adae7
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-06-03 10:52:52 +0100

    feat: file fields validation

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,24 @@
 import {Request, Response} from "express"
+import z from "zod"
+
+import uploadConfig from "@/configs/upload"
 
 class UploadsController {
   async create(request: Request, response: Response){
-    response.json({message: "ok"})
+    try {
+      const fileSchema = z.object({
+        filename: z.string().min(1, {message: "File is required!"}),
+        mimetype: z.string().refine((type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type), {message: `Invalid file format! Allowed formats: ${uploadConfig.ACCEPTED_IMAGE_TYPES}`}),
+        size: z.number().positive().refine((size) => size <= uploadConfig.MAX_FILE_SIZE, {message: `File exceeds maximum size of ${uploadConfig.MAX_SIZE}`})
+      }).passthrough()
+
+      const { file } = fileSchema.parse(request.file)
+
+      response.json({message: "Ok!"})
+
+    } catch (error) {
+      throw error
+    }
   }
 }
 
